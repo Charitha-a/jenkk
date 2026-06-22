ad32e4ace5723b6e5ae0dad12e8829b2d6af64e5
Charitha-a <charitha130305@gmail.com>
2026-06-22 12:08:31 +0530

    1st

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public class App 
+{
+    public int add(int a,int b){
+	return a+b;
+    }
+
+    public static void main( String[] args )
+    {
+	App app=new App();
+	int result=app.add(2,3);
+	System.out.println("the result of add of 2 and 3 :" +result);
+        System.out.println( "App executed success!" );
+    }
+}
---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+
+public class AppTest 
+{
+    @Test
+    public void testApp()
+    {
+	App app=new App();
+	int result=app.add(2,3);
+	System.out.println("the result of add of 2 and 3 ahould be : " + result);
+        Assert.assertEquals(5,result);
+    }
+}
